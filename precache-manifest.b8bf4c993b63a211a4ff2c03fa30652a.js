68f3b4a5ec2ea5855febfbd5d4ac4003ca8c7ed8
Elizabeth Ortiz <ortiz.beth58@gmail.com>
2018-10-12 19:56:50 -0700

    Updates

---
+++ precache-manifest.b8bf4c993b63a211a4ff2c03fa30652a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "832fe04fcc61360d8a31",
+    "url": "/contactmanager/static/js/main.832fe04f.chunk.js"
+  },
+  {
+    "revision": "7bbfc53013fe8eb331d6",
+    "url": "/contactmanager/static/js/1.7bbfc530.chunk.js"
+  },
+  {
+    "revision": "832fe04fcc61360d8a31",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "7bbfc53013fe8eb331d6",
+    "url": "/contactmanager/static/css/1.b10696ff.chunk.css"
+  },
+  {
+    "revision": "a2310f525d80ac3199fce44fc076dfaa",
+    "url": "/contactmanager/index.html"
+  }
+];
